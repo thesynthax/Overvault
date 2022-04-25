472ba785634981d0708be6638b45c3536112b292
Kushagra Srivastava <kushagra1403@gmail.com>
2022-04-25 09:20:32 +0530

    terrain

--- two point five D platformer/Assets/Scripts/New/Camera/CameraControl.cs
+++ two point five D platformer/Assets/Scripts/New/Camera/CameraControl.cs
@@ -15,6 +15,7 @@ public class CameraControl : MonoBehaviour
     public float camMoveSpeed;
     public PlayerMovementBase pMoveBase;
     private Vector3 velocity = Vector3.zero;
+    [SerializeField] private UnityEngine.UI.Toggle camtoggle;
 
     private Transform camPivot;
     private void Start()
@@ -24,42 +25,53 @@ public class CameraControl : MonoBehaviour
 
     private void LateUpdate()
     {
-
-        if (pMoveBase.ragdollControl.ragdollState == RagdollControl.RagdollState.ragdolled)
+        if (!camtoggle.isOn)
         {
-            offset.y = 0.5f;
-
-            target = pMoveBase.anim.GetBoneTransform(HumanBodyBones.Hips);
-
-            float difference = 0f;
-            if (target.position.y > pMoveBase.ragdollControl.hipY)
-            {
-                difference = target.position.y - pMoveBase.ragdollControl.hipY;
-            }
-            else if (target.position.y < (pMoveBase.ragdollControl.hipY - 0.85f))
-            {
-                difference = -(pMoveBase.ragdollControl.hipY - 0.85f - target.position.y);
-            }
-            offset.y += difference;
-
-            Vector3 desiredPosition = new Vector3(target.position.x, camPivot.position.y, target.position.z) + offset;
-            Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, camMoveSpeed);
-            transform.position = smoothedPosition;
-        }
-        else
-        {
-            if (pMoveBase.slideCrouchHandler.UnderObstacleTime() < 0.1f)
+            offset = new Vector3(1.5f, 0.5f, -6);
+            transform.rotation = Quaternion.identity;
+            if (pMoveBase.ragdollControl.ragdollState == RagdollControl.RagdollState.ragdolled)
             {
                 offset.y = 0.5f;
+
+                target = pMoveBase.anim.GetBoneTransform(HumanBodyBones.Hips);
+
+                float difference = 0f;
+                if (target.position.y > pMoveBase.ragdollControl.hipY)
+                {
+                    difference = target.position.y - pMoveBase.ragdollControl.hipY;
+                }
+                else if (target.position.y < (pMoveBase.ragdollControl.hipY - 0.85f))
+                {
+                    difference = -(pMoveBase.ragdollControl.hipY - 0.85f - target.position.y);
+                }
+                offset.y += difference;
+
+                Vector3 desiredPosition = new Vector3(target.position.x, camPivot.position.y, target.position.z) + offset;
+                Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, camMoveSpeed);
+                transform.position = smoothedPosition;
             }
             else
             {
-                offset.y = -0.3f;
+                if (pMoveBase.slideCrouchHandler.UnderObstacleTime() < 0.1f)
+                {
+                    offset.y = 0.5f;
+                }
+                else
+                {
+                    offset.y = -0.3f;
+                }
+                target = camPivot;
+                Vector3 desiredPosition = target.position + offset;
+                Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, camMoveSpeed);
+                transform.position = smoothedPosition;
             }
-            target = camPivot;
+        }
+        else
+        {
+            offset = new Vector3(3.5f, 5, -11);
             Vector3 desiredPosition = target.position + offset;
-            Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, camMoveSpeed);
-            transform.position = smoothedPosition;
+            transform.position = desiredPosition;
+            transform.rotation = Quaternion.Euler(12, 0, 0);
         }
 
     }
